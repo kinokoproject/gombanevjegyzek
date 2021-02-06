6cac045ba59d4918bfdf037391a5a06e4d0c215d
Ferenc Nemeth <ferenc.nemeth@cheppers.com>
2021-02-06 10:51:55 +0100

    update gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   plugins,
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
-      path: path.resolve(__dirname, 'output'),
+      path: path.resolve(__dirname, 'dist'),
       filename: 'bundle.js'
   },
   resolve: {
