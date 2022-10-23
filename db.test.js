fa86770d41b3d09a41b08c1eaa44dc8765f4029f
Ferenc Nemeth <ferenc.nemeth@cheppers.com>
2022-10-24 00:01:50 +0200

    Fix

--- db.test.js
+++ db.test.js
@@ -5,6 +5,7 @@ test('required fields', t => {
   let ids = [];
   let latins = [];
   let huns = [];  
+  let engs = [];
   names.forEach(name => {
 
     t.not(ids.includes(name.id), name.id + ' found twice');
@@ -15,6 +16,9 @@ test('required fields', t => {
 
     t.not(huns.includes(name.hun), name.hun + ' found twice');
     huns.push(name.hun)
+
+    t.not(huns.includes(name.eng), name.eng + ' found twice');
+    engs.push(name.eng)
 
     t.assert(name.hasOwnProperty('id'), name.id + ' has no "id" property');
 
